55dd64a4326c9b8e194d5023c0e914a3193dbcb6
Isus03 <81358094+Isus03@users.noreply.github.com>
2021-04-04 09:52:16 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,60 +1,3 @@
-<?php
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
-
-  require 'vendor/phpmailer/phpmailer/src/Exception.php';
-  require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-  require 'vendor/phpmailer/phpmailer/src/SMTP.php';
-
-  // Include autoload.php file
-  require 'vendor/autoload.php';
-  // Create object of PHPMailer class
-  $mail = new PHPMailer(true);
-
-  $output = '';
-if(isset($_POST['email']) && $_POST['email'] != '') {
-  if (isset($_POST['submit'])) {
-    $ime = $_POST['ime'];
-    $email = $_POST['email'];
-    $podaci1 = $_POST['podaci1'];
-    $podaci2 = $_POST['podaci2'];
-    $kvadratura = $_POST['kvadratura'];
-    $vrijeme = $_POST['vrijeme'];
-    $lokacija = $_POST['lokacija'];
-}
-
-    try {
-      $mail->isSMTP();
-      $mail->Host = 'smtp.gmail.com';
-      $mail->SMTPAuth = true;
-      // Gmail ID which you want to use as SMTP server
-      $mail->Username = 'tor38502003@gmail.com';
-      // Gmail Password
-      $mail->Password = 'Luka2003.';
-      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-      $mail->Port = 587;
-
-      // Email ID from which you want to send the email
-      $mail->setFrom('tor38502003@gmail.com');
-      // Recipient Email ID where you want to receive emails
-      $mail->addAddress('477khii@gmail.com');
-
-      $mail->isHTML(true);
-      $mail->Subject = 'Zahtjev za ponudu';
-      $mail->Body = "<h3>Ime : $ime <br>Email : $email <br>Vrsta posla : $podaci1<br>Opis posla : $podaci2<br>Kvadratura : $kvadratura<br>Vrijeme : $vrijeme<br>Lokacija : $lokacija</h3>";
-
-      $mail->send();
-      $output = '<div class="alert alert-success">
-                  <h5>Thankyou! for contacting us, We\'ll get back to you soon!</h5>
-                </div>';
-    } catch (Exception $e) {
-      $output = '<div class="alert alert-danger">
-                  <h5>' . $e->getMessage() . '</h5>
-                </div>';
-    }
-  }
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -147,3 +90,60 @@ background-color: gray;
 </body>
 
 </html>
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+
+  require 'vendor/phpmailer/phpmailer/src/Exception.php';
+  require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+  // Include autoload.php file
+  require 'vendor/autoload.php';
+  // Create object of PHPMailer class
+  $mail = new PHPMailer(true);
+
+  $output = '';
+if(isset($_POST['email']) && $_POST['email'] != '') {
+  if (isset($_POST['submit'])) {
+    $ime = $_POST['ime'];
+    $email = $_POST['email'];
+    $podaci1 = $_POST['podaci1'];
+    $podaci2 = $_POST['podaci2'];
+    $kvadratura = $_POST['kvadratura'];
+    $vrijeme = $_POST['vrijeme'];
+    $lokacija = $_POST['lokacija'];
+}
+
+    try {
+      $mail->isSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->SMTPAuth = true;
+      // Gmail ID which you want to use as SMTP server
+      $mail->Username = 'tor38502003@gmail.com';
+      // Gmail Password
+      $mail->Password = 'Luka2003.';
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+      $mail->Port = 587;
+
+      // Email ID from which you want to send the email
+      $mail->setFrom('tor38502003@gmail.com');
+      // Recipient Email ID where you want to receive emails
+      $mail->addAddress('477khii@gmail.com');
+
+      $mail->isHTML(true);
+      $mail->Subject = 'Zahtjev za ponudu';
+      $mail->Body = "<h3>Ime : $ime <br>Email : $email <br>Vrsta posla : $podaci1<br>Opis posla : $podaci2<br>Kvadratura : $kvadratura<br>Vrijeme : $vrijeme<br>Lokacija : $lokacija</h3>";
+
+      $mail->send();
+      $output = '<div class="alert alert-success">
+                  <h5>Thankyou! for contacting us, We\'ll get back to you soon!</h5>
+                </div>';
+    } catch (Exception $e) {
+      $output = '<div class="alert alert-danger">
+                  <h5>' . $e->getMessage() . '</h5>
+                </div>';
+    }
+  }
+
+?>
