cc8a20a5564ebe26db33672899baa8cdca04d9b7
Isus03 <81358094+Isus03@users.noreply.github.com>
2021-04-11 18:12:03 +0200

    Update form.php

--- my_site/form.php
+++ my_site/form.php
@@ -1,217 +1,3 @@
-
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>TeamAce</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- Third party plugin CSS-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">TeamAce</a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Masthead-->
-        <header class="masthead">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-10 align-self-end">
-                        <h1 class="text-uppercase text-white font-weight-bold">Are you ready for your own website</h1>
-                        <hr class="divider my-4" />
-                    </div>
-                    <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 font-weight-light mb-5">TeamAce can build you best website for you or your buisness</p>
-                        <a class="btn btn-primary btn-xl js-scroll-trigger bg-dark" href="#about">Find Out More</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- About-->
-        <section class="page-section" id="about" style="background-color:#292b2c">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">We've got what you need!</h2>
-                        <hr class="divider light my-4" />
-                        <p class="text-white-50 mb-4">We are a team of front and back end developers and building sites for our customers for 3 years</p>
-                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Services-->
-        <section class="page-section" id="services">
-            <div class="container">
-                <h2 class="text-center mt-0">At Your Service</h2>
-                <hr class="divider my-4" />
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <i class="fas fa-4x fa-gem text-darkrr mb-4"></i>
-                            <h3 class="h4 mb-2">Sturdy Themes</h3>
-                            <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <i class="fas fa-4x fa-laptop-code text-dark mb-4"></i>
-                            <h3 class="h4 mb-2">Up to Date</h3>
-                            <p class="text-muted mb-0">All our sites are built in period of 3 months</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <i class="fas fa-4x fa-globe text-dark mb-4"></i>
-                            <h3 class="h4 mb-2">Ready to Publish</h3>
-                            <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <i class="fas fa-4x fa-heart text-dark mb-4"></i>
-                            <h3 class="h4 mb-2">Made with Love</h3>
-                            <p class="text-muted mb-0">Is it really open source if it's not made with love?</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Portfolio-->
-        <div id="portfolio">
-            <div class="container-fluid p-0">
-                <div class="row no-gutters">
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="" />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="" />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="" />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="" />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="" />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="" />
-                            <div class="portfolio-box-caption p-3">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Call to action-->
-       
-        <section class="page-section" style="background-color:#292b2c;color: white;" id="contact">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="mt-0">Contact us!</h2>
-                        <hr class="divider my-4" />
-                        <p class="text-muted mb-5" style="font-weight: bold;">Ready to start your next project with us? Send us an email and we will get back to you as soon as possible!</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="name"><b>Name</b></label>
-                    <input type="text" name="ime" id="name" class="form-control" placeholder="Enter your name" required>
-                  </div>
-                    
-             <div class="form-group">
-                    <label for="email"><b>E-Mail</b></label>
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Enter your E-mail" required>
-                  </div>
-            <div class="form-group">
-                <label for="message"><b>Message</b></label>
-                <input type="textarea" name="message" id="message" class="form-control" placeholder="type your message" style="height: 100px;" required>
-            </div>
-            </div>
-          <div class="form-group">
-                <input type="submit" name="submit" value="Send" class="btn btn-dark btn-block" id="sendBtn">
-              </div>
-        </section>
-
-
-
-
-
-
-
-        <!-- Footer-->
-        <footer class="bg-light py-5">
-            <div class="container"><div class="small text-center text-muted">Copyright © 2021 - TeamAce</div></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Third party plugin JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
 <?php
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
@@ -230,8 +16,13 @@ if(isset($_POST['email']) && $_POST['email'] != '') {
   if (isset($_POST['submit'])) {
     $ime = $_POST['ime'];
     $email = $_POST['email'];
-    $message = $_POST['message']
-  }
+    $podaci1 = $_POST['podaci1'];
+    $podaci2 = $_POST['podaci2'];
+    $kvadratura = $_POST['kvadratura'];
+    $vrijeme = $_POST['vrijeme'];
+    $lokacija = $_POST['lokacija'];
+	$telefon = $_POST ['telefon'];
+}
 
     try {
       $mail->isSMTP();
@@ -250,8 +41,8 @@ if(isset($_POST['email']) && $_POST['email'] != '') {
       $mail->addAddress('477khii@gmail.com');
 
       $mail->isHTML(true);
-      $mail->Subject = 'Contact form';
-      $mail->Body = "<h3>Ime : $ime <br>Email : $email <br> Message : $message; </h3>" 
+      $mail->Subject = 'Zahtjev za ponudu';
+      $mail->Body = "<h3>Ime : $ime <br>Email : $email <br> Broj telefona : $telefon<br> Vrsta posla : $podaci1<br>Opis posla : $podaci2<br>Kvadratura : $kvadratura<br>Vrijeme : $vrijeme<br>Lokacija : $lokacija</h3>"; 
 
       $mail->send();
       $output = '<div class="alert alert-success">
@@ -266,3 +57,130 @@ if(isset($_POST['email']) && $_POST['email'] != '') {
 
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kontaktirajte nas</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css' />
+<style>
+
+body {
+	background-image:url(slika123.jpg);
+	background-position: center center;
+	background-repeat: no-repeat;
+	background-attachment: fixed;
+	
+	
+}
+	
+#container2 {
+	background-color: rgba(255 , 255 , 255 , 0.8 );
+	
+	
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+</style>
+
+</head>
+
+<body>
+  <div class="container" >
+    <div class="row justify-content-center">
+      <div class="col-lg-6 mt-3">
+        <div id="container2" >
+          <div class="card-header bg-dark text-light">
+            <h3 class="card-title">Kontaktirajte nas</h3>
+          </div>
+          <div class="card-body px-4">
+            <form action="#" method="POST">
+              <div class="form-group">
+               <?= $output; ?>
+              </div>
+              <div class="form-group">
+                <label for="name"><b>Ime i Prezime</b></label>
+                <input type="text" name="ime" id="name" class="form-control" placeholder="Unesite ime i prezime" required>
+              </div>
+                <div class="form-group">
+                <label for="name"><b>Broj telefona</b></label>
+                <input type="text" name="telefon" id="broj" class="form-control" placeholder="Unesite broj" required>
+              </div>
+		 <div class="form-group">
+                <label for="email"><b>E-Mail</b></label>
+                <input type="email" name="email" id="email" class="form-control" placeholder="Unesite E-Mail" required>
+              </div>
+              <div class="form-group">
+                <label for="c-form-profession">
+                    <span class="label-text"><b>Podaci o poslu</b></span> 
+                    <span class="contact-error"></span>
+                </label>
+                <select name="podaci1" class="c-form-profession form-control" id="c-form-profession">
+                  <option value="-1">Izaberi vrstu posla...</option>
+                  <option value="Adaptacija kuće">Adaptacija kuće</option>
+                  <option value="Adaptacija kupaonice">Adaptacija kupaonice</option>
+                  <option value="Adaptacija stana">Adaptacija stana</option>
+                  <option value="Hidroizolacija temelja, podruma">Hidroizolacija temelja, podruma</option>
+                  <option value="Uređenje potkrovlja">Uređenje potkrovlja</option>
+                  <option value="Hidroizolacija ravnog &nbsp;krova">Hidroizolacija ravnog &nbsp;krova</option>
+                  <option value="Soboslikarski radovi">Soboslikarski radovi</option>
+                </select>
+            </div>
+              <div class="form-group">
+				<label for="subject"><b>Podaci o poslu</b></label>
+                <input type="text" name="podaci2" id="subject" class="form-control" placeholder="Ukratko što trebate"
+                  required>
+              </div>
+              <div class="form-group">
+                <label for="subject"><b>Upišite okvirnu količinu ili kvadraturu</b></label>
+                <input type="text" name="kvadratura" id="subject" class="form-control" placeholder="Kvadratura u m²"
+                  required>
+              </div>
+              <div class="form-group">
+                <label for="c-form-profession">
+                    <span class="label-text"><b>Kada želite početi sa izvedbom</b></span> 
+                    <span class="contact-error"></span>
+                </label>
+                <select name="vrijeme" class="c-form-profession form-control" id="c-form-profession">
+                  <option value="Odmah">Odmah</option>
+                    <option value="do 1 mjeseca">do 1 mjeseca</option>
+                    <option value="do 1-3 mjeseca">do 1-3 mjeseca</option>
+                    <option value="Više od 6 mjeseca">Više od 6 mjeseca</option>
+                    <option value="Zanima me samo okvirna cijena">Zanima me samo okvirna cijena</option>
+                </select>
+            </div>
+            <div class="form-group">
+              <label for="subject"><b>Upišite lokaciju izvedbe radova</b></label>
+              <input type="text" name="lokacija" id="subject" class="form-control" placeholder="Poštanski broj i grad"
+                required>
+            </div>
+              <div class="form-group">
+                <input type="submit" name="submit" value="Pošalji" class="btn btn-dark btn-block" id="sendBtn">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
